332ec85c0a5c9da5ec8c748c2ef43db7aba455d0
unknown <Eduardo Filip@ITJ0019.itajai.quay.com.br>
2020-12-28 16:16:38 -0300

    Movimentacao

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); // Context renderiza o Canvas, "getContext 2d" trata o canvas como 2D //
 let box = 32; // 32 pixels cada quadrado//
+
 let snake = [];
-snake[0] = {
+    snake[0] = {
     x: 8 * box,
     y: 8 * box
 } // Posicao de inicio //
 
-
+let direction = "right";
 
 function criarBG(){ // Funcao para definir background //
     context.fillStyle = "lightgreen"; // fillStyle trabalha com o Context do Canvas //
@@ -21,5 +22,32 @@ function criarCobrinha (){
     } // tamanho de x e y setado lá em cima no let snake, o box setado em let box - tamanho de um quadradinho //
 }
 
+function iniciarJogo(){
 criarBG();
 criarCobrinha();
+
+let snakeX = snake[0].x; //Posicao ZERO de X // 
+let snakeY = snake[0].y; // Posicao ZERO de Y //
+
+//Coordenas para movimentacao //
+
+if (direction == "right") snakeX += box; // Se a direcao for direita, aumenta um box //
+
+if (direction == "left") snakeX -= box; // Se a direcao for esquerda, reduz um box //
+
+if (direction == "up") snakeY -= box; // Se a direcao for para cima, reduz um box //
+
+if (direction == "down") snakeY += box; // Se a direcao for para baixo, acrescenta um box //
+
+snake.pop(); // funcao que retira o ultimo elemento do array //
+
+let newHead = {
+    x: snakeX,
+    y: snakeY
+    }
+
+    snake.unshift(newHead); // metodo unshift, acrescenta no primeiro lugar //
+
+}
+
+let jogo = setInterval(iniciarJogo,100); // Irá iniciar o jogo após 100 milisegundos //
