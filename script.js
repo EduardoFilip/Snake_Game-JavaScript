5724ec86ddb36b33924ad1364cf8078db0f1e5fc
unknown <Eduardo Filip@ITJ0019.itajai.quay.com.br>
2020-12-29 16:10:20 -0300

    Criando a comida

--- script.js
+++ script.js
@@ -10,20 +10,48 @@ let snake = [];
 
 let direction = "right";
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box, // Math.floor irá retornar um número INTEIRO que esteja dentro de Math.random. 
+    
+    //Math.random irá gerar um número aleatório entre ZERO e UM.
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+
+
+
 
 // Criar Background //
+
 function criarBG(){ // Funcao para definir background //
     context.fillStyle = "lightgreen"; // fillStyle trabalha com o Context do Canvas //
     context.fillRect(0, 0, 16 * box, 16 * box); // fillRect, irá desenhar o "retangulo", onde irá ocorrer o game **** Trabalha com 4 parametros: posicao de X e Y, altura e largura//
 }
 
+
+
+
+
 // Criar Cobra //
+
 function criarCobrinha (){
     for(i=0; i < snake.length; i++){ // Vai percorrer todo o tamanho do array e vai incrementar //
     context.fillStyle = "green"; // Setando a cor verde //
     context.fillRect(snake[i].x, snake[i].y, box, box);
     } // tamanho de x e y setado lá em cima no let snake, o box setado em let box - tamanho de um quadradinho //
 }
+
+
+// Comida //
+function drawFood(){
+    context.fillStyle = "red"; // Cor da comida //
+    context.fillRect(food.x,food.y, box, box); // Definir a localizacao da comida
+}
+
+
+
+
+// Movimentacao //
 
 document.addEventListener('keydown', update); // Evento listener, irá chamar a funcao update quando o clique no keydown for realizado //
 
@@ -33,7 +61,6 @@ function update (event) {
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down"; 
 }
-
 
 
 
@@ -49,6 +76,7 @@ function iniciarJogo(){
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box; // Se a posicao ZERO do snake for menor que 0 na direcao up, a posicao ZERO do snake recebera ZERO novamente.
 criarBG();
 criarCobrinha();
+drawFood();
 
 let snakeX = snake[0].x; //Posicao ZERO de X // 
 let snakeY = snake[0].y; // Posicao ZERO de Y //
